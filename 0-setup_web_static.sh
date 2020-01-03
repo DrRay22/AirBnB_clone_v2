eb70599ebc2da7ae53401a0b08641af62b76ae76
luischaparroc <lfchaparro@outlook.com>
2020-01-03 17:02:50 -0500

    FIx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,6 +9,6 @@ mkdir -p /data/web_static/shared
 echo "Hello World" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}\n/" /etc/nginx/sites-available/default
+sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t}\n/" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
