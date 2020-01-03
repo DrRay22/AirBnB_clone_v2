831065c46d3f88cb77bada232608b4b8c543d86b
luischaparroc <lfchaparro@outlook.com>
2020-01-03 15:55:50 -0500

    [Feature]: adding a script that sets a server New file: 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Hello World" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/index.html;\n\t}/" /etc/nginx/sites-available/default
+service nginx restart
