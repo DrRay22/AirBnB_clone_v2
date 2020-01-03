068ce15d8912654583e657dcf67f0c9058d923ef
luischaparroc <lfchaparro@outlook.com>
2020-01-03 17:07:30 -0500

    Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,10 @@ apt-get -y update
 apt-get -y install nginx
 sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf
 service nginx start
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "Hello World" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t}\n/" /etc/nginx/sites-available/default
 service nginx restart
