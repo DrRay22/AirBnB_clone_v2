4398c520a75be5709590514722d6604e0da12727
luischaparroc <lfchaparro@outlook.com>
2020-01-03 16:40:58 -0500

    FIx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,13 @@
 # Script that configures Nginx server with some folders and files
 apt-get -y update
 apt-get -y install nginx
+sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf
 service nginx start
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
 echo "Hello World" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /data/
 sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n\t}/" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
