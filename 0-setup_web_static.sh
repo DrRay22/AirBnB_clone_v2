44c45de272be707fe9583e05c420771e9a1f2c8f
luischaparroc <lfchaparro@outlook.com>
2020-01-03 17:10:16 -0500

    FIx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-apt-get -y update
-apt-get -y install nginx
-sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello World" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i "s/server_name _;/server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t}\n/" /etc/nginx/sites-available/default
-service nginx restart
+# Install nginX and configure the http header
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo sed -i "15i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf
+
+# Create directories
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+
+# Create html file
+echo "<html>
+<head>
+</head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+# Create the symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Set-up the content of /data/web_static/current/ to redirect
+# to domain.tech/hbnb_static
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart nginx
+sudo service nginx restart
 exit 0
