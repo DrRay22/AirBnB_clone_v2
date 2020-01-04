62988cc2c3d2245e80b1bab90d2d9dd62b5020a0
luischaparroc <lfchaparro@outlook.com>
2020-01-03 22:06:02 -0500

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,10 +11,9 @@ env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-
+    """ Deploys """
     if not os.path.exists(archive_path):
         return False
-
     try:
         put(archive_path, "/tmp/")
 
@@ -33,7 +32,7 @@ def do_deploy(archive_path):
         run("tar -xzf {} -C {}".format(tmp_path, releases_path))
         run("rm {}".format(tmp_path))
         run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}/web_static".format(releases_path))
+        run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static_current".format(releases_path))
         print("New version deployed!")
