55f3baa6c8ff8ff80a51d4b866583c7b519e926d
luischaparroc <lfchaparro@outlook.com>
2020-01-03 22:51:31 -0500

    Adding script

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function that deploys """
+from fabric.api import *
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
+
+
+def deploy():
+    """ DEPLOYS """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
